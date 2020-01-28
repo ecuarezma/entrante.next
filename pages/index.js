7f1b194efe972d8b49ea36de566932dc135754c7
Edgar Cuarezma <ecuarezma@gmail.com>
2020-01-28 17:17:43 -0600

    Add image and text to homepage

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,15 @@ import React from "react";
 import image from "../components/images";
 
 const Homepage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <header>
+        <h1>Edgar Cuarezma</h1>
+      </header>
+      <img src={`../images/home/${image.home}`} />
+      <h2>Fine Art Illustration</h2>
+    </div>
+  );
 };
 
 export default Homepage;
