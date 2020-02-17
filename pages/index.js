5ca30259406dc01cb44855c3f25a9b4e0029008d
Edgar Cuarezma <ecuarezma@gmail.com>
2020-02-16 21:02:35 -0600

    Begin styling navbar and homepage

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
 import React from "react";
 import images from "../components/utilities/images";
 
+const style = {
+  main: {
+    display: "flex",
+    flexFlow: "column nowrap",
+    alignItems: "center",
+    margin: "0 4rem"
+  },
+  image: {
+    width: "100%"
+  }
+};
+
 const Homepage = () => {
   return (
-    <div>
+    <div style={style.main}>
       <header>
         <h1>Edgar Cuarezma</h1>
       </header>
-      <img src={`../images/home/${images.home}`} />
+      <img style={style.image} src={`../images/home/${images.home}`} />
       <h2>Fine Art Illustration</h2>
     </div>
   );
